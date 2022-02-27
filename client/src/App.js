5a39b20909eb0b11f7e9b229831c859636c0fda4
chefyeum <chefyeum@gmail.com>
2022-02-27 01:38:57 +0000

    delete legacy

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-/* global document */
 import * as React from 'react';
 import Map, { Marker, Popup } from 'react-map-gl';
 import ReactPlayer from 'react-player';
@@ -33,39 +32,3 @@ export default function App() {
       </Map>
     </div>);
 }
-
-
-// export default function App() {
-//   const mapContainer = useRef(null);
-//   const map = useRef(null);
-//   const [lng, setLng] = useState(-70.9);
-//   const [lat, setLat] = useState(42.35);
-//   const [zoom, setZoom] = useState(9);
-
-//   useEffect(() => {
-//     if (map.current) return; // initialize map only once
-//     map.current = new mapboxgl.Map({
-//       container: mapContainer.current,
-//       style: 'mapbox://styles/mapbox/streets-v11',
-//       center: [lng, lat],
-//       zoom: zoom
-//     });
-//   });
-
-//   useEffect(() => {
-//     if (!map.current) return; // wait for map to initialize
-//     map.current.on('move', () => {
-//       setLng(map.current.getCenter().lng.toFixed(4));
-//       setLat(map.current.getCenter().lat.toFixed(4));
-//       setZoom(map.current.getZoom().toFixed(2));
-//     });
-//   });
-
-//   return (
-//     <div>
-//       <div className="sidebar">
-//         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-//       </div>
-//     </div>
-//   );
-// }
