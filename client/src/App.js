2ac06f7f4c94ea2bea2b6a96a884d7db5746a889
chefyeum <chefyeum@gmail.com>
2022-02-27 02:02:02 +0000

    add video clip popup component

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,21 @@ import ReactPlayer from 'react-player';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2hlZnlldW0iLCJhIjoiY2wwNDQ2ZjFxMGR4czNxcGRxdDlsODQwdCJ9.j3HMK_j8BvB-EpAMe76wAQ';
 
-
 const UA_COORDINATES = {
   'latitude': 49.34051271789116,
   'longitude': 31.21759249962616
 };
+
+
+const VideoClipPopup = ({setShowPopUp, sourceUrl}) => (
+  <Popup longitude={UA_COORDINATES.longitude} latitude={UA_COORDINATES.latitude}
+    anchor="bottom"
+    onClose={() => setShowPopUp(false)}>
+    Video goes here
+    <ReactPlayer url={sourceUrl}
+      width='6em' height='6em' />
+  </Popup>
+);
 
 export default function App() {
   const [showPopup, setShowPopup] = React.useState(true);
@@ -24,12 +34,7 @@ export default function App() {
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={MAPBOX_TOKEN}
       >
-        <Popup longitude={UA_COORDINATES.longitude} latitude={UA_COORDINATES.latitude}
-          anchor="bottom"
-          onClose={() => setShowPopup(false)}>
-          Video goes here
-          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-        </Popup>
+        <VideoClipPopup setShowPopUp={setShowPopup} sourceUrl={"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"} />
       </Map>
     </div>);
 }
